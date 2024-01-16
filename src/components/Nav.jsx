32ea273a27e1744ca72be5455a771ba75d78be60
Serhii Didenko <srg.dd0@gmail.com>
2024-01-16 15:54:42 +0200

    init

---
+++ src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import HeaderLogo from '../assets/images/header-logo.svg';
+import Hamburger from '../assets/icons/hamburger.svg';
+
+import { navLinks } from '../constants';
+import { hamburger } from '../assets/icons';
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img
+            src={HeaderLogo}
+            alt='Logo'
+            width={130}
+            height={29}
+          />
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map(item => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray'
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='hidden max-lg:block'>
+          <img
+            src={hamburger}
+            alt='hamburger'
+            width={25}
+            height={25}
+          />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
