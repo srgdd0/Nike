6c6497121ac8fb57a82c8e95908ba45e8a9ab2e2
Serhii Didenko <srg.dd0@gmail.com>
2024-01-16 16:21:09 +0200

    feat: popular Products section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,3 +1,4 @@
+import PopularProductsCard from '../components/PopularProductsCard';
 import { products } from '../constants/';
 
 const PopularProducts = () => {
@@ -14,6 +15,14 @@ const PopularProducts = () => {
           Experience top-notch quality and style with our sought-after selections. Discover a world
           of comfort, design, and value
         </p>
+      </div>
+      <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14'>
+        {products.map(product => (
+          <PopularProductsCard
+            key={product.name}
+            {...product}
+          />
+        ))}
       </div>
     </section>
   );
